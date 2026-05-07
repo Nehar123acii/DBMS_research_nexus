8df63ca565f836a00bea238b9eb564387c7bfd23
Nehar123acii <neharaci@gmail.com>
2026-05-07 20:35:04 +0530

    Fix postgres connection timeout and ssl

--- config/postgres.js
+++ config/postgres.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.PG_URI,
+    ssl: process.env.PG_URI ? { rejectUnauthorized: false } : false,
+    connectionTimeoutMillis: 5000,
+    query_timeout: 5000
 });
 
 pool.on('error', (err, client) => {
